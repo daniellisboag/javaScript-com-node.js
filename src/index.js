7f946f772c71829315ad2ff2acbe434faf60d607
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-07-23 16:53:03 -0300

    Capturando as palavras

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) => {
-   console.log(texto);
+   verificaPalavrasDuplicadas(texto);
 });
 
-console.log(link);
+
+// Criar um array com as palavras.
+// Contar as ocorrências.
+// Montar um objeto com o resultado.
+
+/* {
+   'web': 5,
+   'computador': 4
+} */
+
+function verificaPalavrasDuplicadas(texto) {
+   const listaPalavras = texto.split(' ');
+   const resultado = {};
+   // Objeto[propriedade] = valor;
+   listaPalavras.forEach(palavra => {
+      resultado[palavra] = (resultado[palavra] || 0) + 1;
+   });
+   console.log(resultado);
+}
